aa0bcf264a1031137380799c4278119ae86b79cd
Leonid Taranov <supertaranov@gmail.com>
2023-05-05 22:27:58 +0700

    update

---
+++ 2. Stepik - Python for advanced [2023]/Tuples [13]/14. Total.py
@@ -0,0 +1,7 @@
+# Complete the above code so that it outputs the product of tuple numbers
+
+numbers = (2, 3, 5, 7, -11, 13, 17, 19, 23, 29, 31, -6, 41, 43, 47, 53, 59, 61, -96, 71, 1000, -1)
+total = 1
+for i in numbers:
+    total *= i
+print(total)
