b3f7d177d7a55b64832ad0c9a266140cf6abe2ba
Leonid Taranov <supertaranov@gmail.com>
2023-05-15 23:06:04 +0700

    update

---
+++ 2. Stepik - Python for advanced [2023]/Set/2. Max and Min.py
@@ -0,0 +1,6 @@
+# Complete the above code so that it prints the sum of the minimum 
+# and maximum elements of the numbers
+
+numbers = {1.414, 12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324, 2.718, 1.324}
+
+print(min(numbers) + max(numbers))
---
+++ 2. Stepik - Python for advanced [2023]/Set/3. Sum and Len.py
@@ -0,0 +1,6 @@
+# Complete the above code so that it prints the arithmetic mean of the elements of a set of numbers
+
+numbers = {20, 6, 8, 18, 18, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 12, 8, 8, 10, 4, 2, 2, 2, 16, 20}
+average = len(numbers)
+
+print(sum(numbers) / average)
