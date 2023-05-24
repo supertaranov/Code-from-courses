ebdea2d234a2d2e16a97d260c6eac83db51d43c2
Leonid Taranov <supertaranov@gmail.com>
2023-05-24 17:54:42 +0700

    update

---
+++ 2. Stepik - Python for advanced [2023]/Dict/1. Min + Max.py
@@ -0,0 +1,16 @@
+# Supplement the above code so that it outputs the sum of minimum and maximum keys in my_dict dictionary.
+
+my_dict = {
+    1.12: 'aa', 
+    67.9: 45, 
+    3.11: 'ccc', 
+    7.9: 'dd', 
+    9.2: 'ee', 
+    7.1: 'ff', 
+    0.12: 'qq', 
+    1.91: 'aa', 
+    10.12: [1, 2, 3], 
+    99.0: {9, 0, 1}
+    }
+
+print(min(my_dict) + max(my_dict))
