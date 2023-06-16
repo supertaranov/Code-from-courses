b7b6f262085d8ab3072cfc0a440b8d65383e63ca
Leonid Taranov <supertaranov@gmail.com>
2023-06-16 15:48:49 +0700

    update

---
+++ 1. Stepik - Python for beginners [2022]/5. Int and float/7. Fractional part.py
@@ -0,0 +1,11 @@
+# A positive real number is given. Output its fractional part.
+
+# Input data format:
+# The input to the program is a positive real number.
+
+# The format of the output data:
+# The program has to print the fractional part of the number according to the problem.
+
+num = float(input())
+x = int(num)
+print(num - x)
