fe5e04bb72b7386b8456acb27ef8f34c999f5b44
Leonid Taranov <supertaranov@gmail.com>
2023-06-28 11:19:24 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/6. Str/6. Blue.py
@@ -0,0 +1,13 @@
+# Write a program that reads one line, then outputs "YES" if the entered line has a substring "blue" and "NO" otherwise.
+
+# Input Data Format:
+# The program takes one string as input.
+
+# Output data format:
+# The program should output the text according to the problem's condition.
+
+x = input()
+if 'blue' in x:
+    print('YES')
+else:
+    print('NO')
