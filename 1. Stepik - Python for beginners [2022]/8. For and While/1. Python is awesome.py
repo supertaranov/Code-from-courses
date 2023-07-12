abdfad682b2548b7b77cb9fa045cd0cb639a5977
Leonid Taranov <supertaranov@gmail.com>
2023-07-12 10:04:23 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/8. For and While/1. Python is awesome.py
@@ -0,0 +1,7 @@
+# Write a program that outputs the words "Python is awesome!" (without quotes) 10 times.
+
+# Output format:
+# The program should output the text "Python is awesome!" 10 times, each on a separate line.
+
+for i in range(10):
+    print('Python is awesome!')
