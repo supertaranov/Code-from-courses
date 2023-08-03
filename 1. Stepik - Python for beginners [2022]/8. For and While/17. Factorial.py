486207116dc15473a359b66eaa41a13c6e0d6608
Leonid Taranov <supertaranov@gmail.com>
2023-08-03 10:27:43 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/8. For and While/17. Factorial.py
@@ -0,0 +1,11 @@
+# A natural number n is given as input to the program. Write a program that calculates n!.
+
+# Input data:
+# A natural number n,(n≤12) is given to the input of the program.
+
+# Output data:
+# The program should output a single number according to the problem condition.
+
+from math import *
+n = int(input())
+print(factorial(n))
