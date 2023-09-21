714e38fb4d630386f86572ab6cbfe3326fc20e91
Leonid Taranov <supertaranov@gmail.com>
2023-09-21 14:36:00 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/Nested loops/7. Numerical triangle 3.py
@@ -0,0 +1,24 @@
+# Given a natural number n. 
+# Write a program that prints a numerical triangle with height equal to n, according to the example:
+# 1
+# 121
+# 12321
+# 1234321
+# 123454321
+# ...
+
+# Input data format:
+# The program receives one natural number as input.
+
+# Output data format:
+# The program should output a triangle according to the condition.
+
+# Note. Use a nested for loop.
+
+n = int(input())
+for i in range(n):
+    for j in range(i + 1):
+        print(j+1, end='')
+    for j in range(i, 0, -1):
+        print(j, end='')
+    print()
