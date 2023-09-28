285a2699088031017325a47d27d86c363c75cd11
Leonid Taranov <supertaranov@gmail.com>
2023-09-28 10:04:56 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/12. Indexing/5. Surname.py
@@ -0,0 +1,15 @@
+# The program is given three strings as input: first name, last name, and patronymic. 
+# Write a program that outputs the initials of a person.
+
+# Input data format:
+# The program is given three strings as input, each on a separate line.
+
+# Output data format:
+# The program should output the person's full name.
+
+# Note. It is guaranteed that first name, last name and patronymic begin with a capital letter.
+
+a = input()
+b = input()
+c = input()
+print(b[0] + a[0] + c[0])
