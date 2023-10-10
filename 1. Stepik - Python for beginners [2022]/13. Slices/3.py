b6635e29d8ed0c9042c0d8531e8c1e9976d2f3ae
Leonid Taranov <supertaranov@gmail.com>
2023-10-10 08:41:43 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/13. Slices/3.py
@@ -0,0 +1,4 @@
+# Complete the above code by using slices so that it outputs every 7 characters of string s (starting from the 0th index).
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::7])
