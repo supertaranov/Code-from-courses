052429cdec59948498259443df2c5dae66ca10fd
Leonid Taranov <supertaranov@gmail.com>
2023-10-19 08:37:21 +0100

    update

---
+++ 1. Stepik - Python for beginners [2022]/14. String methods.py/2. sWAP cASE.py
@@ -0,0 +1,12 @@
+# The input to the program is a string. 
+# Write a program that changes the case of characters, in other words, 
+# replace all lowercase characters with uppercase characters and vice versa.
+
+# Input data format: 
+# The input to the program is a string.
+
+# Output data format:
+# The program should output the string according to the problem condition.
+
+t = input()
+print(t.swapcase())
