d75f86391d7b9397962b01557ffb81ec46456f9b
Leonid Taranov <supertaranov@gmail.com>
2023-11-06 09:41:18 +0000

    update

---
+++ 1. Stepik - Python for beginners [2022]/14. String methods.py/14. Bitcoin-2.py
@@ -0,0 +1,8 @@
+# Complete the above code using f-string formatting so that it outputs the text: 
+# "In 2010, someone paid 10K Bitcoin for two pizzas." (without quotes).
+
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
