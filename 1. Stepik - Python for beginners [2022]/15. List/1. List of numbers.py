554a84d5cd6abb5581627d290f2fdc583a17ab00
Leonid Taranov <supertaranov@gmail.com>
2023-11-10 09:46:13 +0000

    update

---
+++ 1. Stepik - Python for beginners [2022]/15. List/1. List of numbers.py
@@ -0,0 +1,11 @@
+# The program receives one number as input n. 
+# Write a program that outputs the list [1, 2, 3, ..., n].
+
+# Input data format:
+# One natural number is given as input to the program.
+
+# Output data format:
+# The program should output the text according to the problem condition.
+
+n = int(input())
+print(list(range(1, n + 1)))
