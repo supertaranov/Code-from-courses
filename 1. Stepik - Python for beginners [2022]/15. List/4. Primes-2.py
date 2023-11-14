dc51599b883ec5788b7c3f1e309bb6a4cfbc89ed
Leonid Taranov <supertaranov@gmail.com>
2023-11-14 09:15:03 +0000

    update

---
+++ 1. Stepik - Python for beginners [2022]/15. List/4. Primes-2.py
@@ -0,0 +1,5 @@
+# Complete the above code using an indexer so that it outputs the last element of the primes list
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+
+print(primes[-1])
