86d30d8c4ae0c9defb62adfb55c954e4df51e5bb
Leonid Taranov <supertaranov@gmail.com>
2023-11-15 09:12:22 +0000

    update

---
+++ 1. Stepik - Python for beginners [2022]/15. List/5. Primes-3.py
@@ -0,0 +1,7 @@
+# Complete the above code using slices so that it outputs the first 6 elements of the primes list.
+
+# Note. The output result should be the string [2, 3, 5, 7, 11, 13].
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+
+print(primes[0:6])
