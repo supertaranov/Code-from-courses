00b0b9794c16d005403bc17fa985a337cdc7e0ef
Leonid Taranov <supertaranov@gmail.com>
2023-11-16 07:49:01 +0000

    update

---
+++ 1. Stepik - Python for beginners [2022]/15. List/6. Numbers.py
@@ -0,0 +1,5 @@
+# Supplement the above code so that it outputs the sum of the minimum and maximum elements of the numbers list.
+
+numbers = [12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324]
+
+print(min(numbers) + max(numbers))
