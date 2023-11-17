5e76499795487043a6892981e2f9e8715bd5d682
Leonid Taranov <supertaranov@gmail.com>
2023-11-17 08:13:45 +0000

    update

---
+++ 1. Stepik - Python for beginners [2022]/15. List/7. Evens.py
@@ -0,0 +1,6 @@
+# Supplement the above code so that it outputs the arithmetic mean of the elements of the evens list.
+
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+average = sum(evens) / len(evens)
+
+print(average)
